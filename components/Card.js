85e15ae5563f360d6b23b198484119d34339b160
Adam el Hassan <ahassan3@avans.nl>
2019-01-13 20:47:18 +0100

    changed from 2 spaces to 4

--- components/Card.js
+++ components/Card.js
@@ -1,47 +1,47 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  View,
-  Text,
+    StyleSheet,
+    View,
+    Text,
 } from 'react-native';
 
 export default class Card extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <View style={styles.card}>
-        <Text style={styles.text}>{this.props.value}</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.card}>
+                <Text style={styles.text}>{this.props.value}</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-    height: 80,
-    width: 50,
-    textAlign: 'center',
-    lineHeight: 80,
-  },
-  card: {
-    marginTop: 20,
-    marginHorizontal: 4,
-    borderRadius: 6,
-    height: 80,
-    width: 50,
-    backgroundColor: 'rgb(0,200,200)',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
+    text: {
+        fontSize: 30,
+        height: 80,
+        width: 50,
+        textAlign: 'center',
+        lineHeight: 80,
     },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
-  }
+    card: {
+        marginTop: 20,
+        marginHorizontal: 4,
+        borderRadius: 6,
+        height: 80,
+        width: 50,
+        backgroundColor: 'rgb(0,200,200)',
+        shadowColor: "#000",
+        shadowOffset: {
+        width: 0,
+        height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
+    }
 });
--- components/Hand.js
+++ components/Hand.js
@@ -4,37 +4,37 @@ import { StyleSheet, FlatList } from 'react-native';
 
 export default class Hand extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  renderCard = (card) => {
-    return (
-        <Card style={styles.cardContainer}
-        value = '10'/>
-    );
-  }
+    renderCard = (card) => {
+        return (
+            <Card style={styles.cardContainer}
+            value = '10'/>
+        );
+    }
 
-  render() {
-    return (
-        <FlatList style={styles.hand}
-            horizontal
-            data={this.props.cards}
-            renderItem={({ card }) => this.renderCard(card)}
-            keyExtractor={this._keyExtractor}
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{flexGrow: 1, justifyContent: 'center'}}
-      />
-    );
-  }
+    render() {
+        return (
+            <FlatList style={styles.hand}
+                horizontal
+                data={this.props.cards}
+                renderItem={({ card }) => this.renderCard(card)}
+                keyExtractor={this._keyExtractor}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{flexGrow: 1, justifyContent: 'center'}}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  hand: {
-    position: 'absolute',
-    bottom: 0,
-    height: 120,
-    width: '100%',
-    backgroundColor: 'rgb(200,200,200)',
-  }
+    hand: {
+        position: 'absolute',
+        bottom: 0,
+        height: 120,
+        width: '100%',
+        backgroundColor: 'rgb(200,200,200)',
+    }
 });
